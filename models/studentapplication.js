af3d85d0cdf418c92c0de7c0e1daa6fd490e9ff9
senth542002 <senth542002@gmail.com>
2019-10-09 10:10:42 +0530

    Added new endpoints to search by Application number and mobile number

--- models/studentapplication.js
+++ models/studentapplication.js
@@ -1,6 +1,7 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const StudentApplication = sequelize.define('StudentApplication', {
+
     name: {
       type: DataTypes.STRING,
       allowNull: false,
